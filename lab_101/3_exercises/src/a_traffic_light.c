3ea3a72bf918067ed57fa4986c2952818621b1b7
huwslocombe <43818148+huwslocombe@users.noreply.github.com>
2018-11-08 14:59:02 +0000

    blinky thread done

---
+++ lab_101/3_exercises/src/a_traffic_light.c
@@ -0,0 +1,27 @@
+/*
+ * a_traffic_lights.c
+ *
+ * this is the main application skeleton for the student exercises in task 3
+ *
+ */
+
+// include the hal drivers
+#include "stm32f7xx_hal.h"
+
+// include the shu bsp libraries for the stm32f7 discovery board
+#include "pinmappings.h"
+#include "clock.h"
+#include "gpio.h"
+
+// map the led to gpio pins
+gpio_pin_t led1 = {PA_8,  GPIOA, GPIO_PIN_8};
+
+// this is the main method
+int main()
+{
+  // we need to initialise the hal library and set up the SystemCoreClock 
+  // properly
+  HAL_Init();
+  init_sysclk_216MHz();
+
+}
